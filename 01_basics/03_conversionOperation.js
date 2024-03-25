4460586f53b67f2d85db333a6068cb20be9adc50
ronitjacob <164862978+ronitjacob@users.noreply.github.com>
2024-03-25 14:32:09 +0000

    COnversionOperation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -21,5 +21,25 @@ let booleanisLoggedIn = Boolean(isLoggedIn)
 
 let SomeNumber = 33
 let stringNumber = String(SomeNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+// ***************** Operations ***************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+let str1 = "hello"
+let str2 = "ronit"
+
+let str3 = str1 + str2
+//console.log(str3);
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
