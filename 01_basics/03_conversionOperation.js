cab0fb832e6f0823425ff66447245c370c70f42f
ronitjacob <164862978+ronitjacob@users.noreply.github.com>
2024-03-25 14:03:18 +0000

    COnversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = "33"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true =>1; false => 0
+
+let isLoggedIn = 1
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanisLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "ronit" => true
+
+let SomeNumber = 33
+let stringNumber = String(SomeNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
