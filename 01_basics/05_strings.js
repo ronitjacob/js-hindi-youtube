053f11d37169518c6e24f3912202f1c246701ea5
ronitjacob <164862978+ronitjacob@users.noreply.github.com>
2024-03-26 08:55:36 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,17 @@
+const name = "ronit"
+const repoCount = 50
+
+console.log(name + repoCount + "Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('ronit')
+
+console.log(gameName[0]);
+console.log(gameName._proto_);
+
+//console.log(gameName.length);
+//console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
