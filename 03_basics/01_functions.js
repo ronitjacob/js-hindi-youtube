9f88ff6ec9b2c7da345b5277f77e47abba341b6c
ronitjacob <164862978+ronitjacob@users.noreply.github.com>
2024-04-01 07:54:10 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,11 +1,11 @@
 
-function sayMyName(){
-    console.log("R");
-    console.log("O");
-    console.log("N");
-    console.log("I");
-    console.log("T");
-}
+// function sayMyName(){
+//     console.log("R");
+//     console.log("O");
+//     console.log("N");
+//     console.log("I");
+//     console.log("T");
+// }
 
 //sayMyName()
 
@@ -13,28 +13,53 @@ function sayMyName(){
 //    console.log(number1 + number2);
 // }
 
-function addTwoNumbers(nnumber1, number2){
+// function addTwoNumbers(nnumber1, number2){
 
 
-    // let result = number1 + number2
-    // return result
-    return number1 + number2
- }
+//     // let result = number1 + number2
+//     // return result
+//     return number1 + number2
+//  }
 
-const result = addTwoNumbers(3, 5)
+// const result = addTwoNumbers(3, 5)
 
 // console.log("Result:", result);
 
-function loginUserMessage(username ="sam"){
-    if(!username){
-    console.log("Please enter a username");
-    return
-    }
-    return `${username} just logged in`
-}
+// function loginUserMessage(username ="sam"){
+//     if(!username){
+//     console.log("Please enter a username");
+//     return
+//     }
+//     return `${username} just logged in`
+// }
 
 // console.log(loginUserMessage("ronit"));
-console.log(loginUserMessage());
+// console.log(loginUserMessage());
 
 
+function calculateCartPrice(...num1){
+   return num1
+}
+ 
+// console.log(calculateCartPrice(200, 400, 500));
+//  const user = {
+//     username: "Ronit",
+//     price: 199
+//  }
 
+//  function handleObject(user){
+//     console.log(`Username is ${anyobejct.username} and price is ${anyobejct.price}`);
+//  }
+
+//  handleObject(user)
+ handleObject({
+    username: "sam",
+    price:399
+ })
+ 
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+console.log(returnSecondValue(myNewArray));
