a53c002740d968d7e15d80679a45a7d319c9863e
ronitjacob <164862978+ronitjacob@users.noreply.github.com>
2024-04-06 07:36:12 +0000

    one

---
+++ 04_control_flow/one.js
@@ -0,0 +1,26 @@
+// if
+const isUserLoggedIn = true
+const temperature = 41
+
+// if( temperature === 41){
+//      console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50");
+// }
+// console.log("Execute");
+
+// <, >, <=, >=, ==, !=, ===, !==
+
+// const score = 200
+
+// if (score > 100) {
+//       const power = "fly"
+//       console.log(`User power: ${power}`);
+// }
+
+//  console.log(`User power: ${power}`);
+
+const balance = 1000
+
+// if (balance > 500) console.log("test");
+// console.log("test2");
