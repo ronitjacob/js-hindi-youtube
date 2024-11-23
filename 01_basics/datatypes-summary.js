8cc19969638ca2de11b6a640f2033b990d8c977e
ronitjacob <164862978+ronitjacob@users.noreply.github.com>
2024-11-23 15:47:43 +0000

    datatyps

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -8,28 +8,30 @@ const scoreValue = 100.3
 
 const isLoggedIn = false
 const outsideTemp = null
-let userEmail;
+let userEmail; //undefined
 
 const id = Symbol('123')
-const anotherId = Symbol('123')
-
+const anotherId= Symbol('123')
 console.log(id === anotherId);
 
-const bigNumber = 34565476767657n
+ const bigNumber = 34567890123456n
 
 
-// Reference (Non Primitive)
+// Reference (Non primitve)
 
-// Array, Objects, Functions
+// Array, objects, Functions
 
 const heros = ["shaktiman", "naagraj", "doga"]
+
  let myObj = {
     name: "ronit",
-    age: 24,
+    age:24,
 }
 
 const myFunction = function(){
     console.log("Hello world");
+    
 }
 
-console.log(typeof bigNumber);
+
+console.log(typeof scoreValue);
