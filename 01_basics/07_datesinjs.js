32b458a049d32f4d62469575f0dab741983dd1d3
ronitjacob <164862978+ronitjacob@users.noreply.github.com>
2024-11-24 15:53:51 +0000

    dateinjs

--- 01_basics/07_datesinjs.js
+++ 01_basics/07_datesinjs.js
@@ -1,18 +1,22 @@
 // Dates
 
 let myDate = new Date()
-//console.log(myDate.toString());
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
 
-// let myCreatedDate = new Date(2024, 0 ,23)
-// console.log(myCreatedDate.toDateString());
+// let myCreatedDate = new Date(2023,0,23)
+let myCreatedDate = new Date("01-14-2024")
+// console.log(myCreatedDate.toLocaleString());
 
-let myCreatedDate = new Date("01-14-1014")
-// console.log(myCreatedDate.toLocaleDateString());
+let myTimeStamp = Date.now()
 
-// let myTimeStamp = Date.now()
 // console.log(myTimeStamp);
 // console.log(myCreatedDate.getTime());
-// console.log(Math.floor(Date.now()/1000));
+// console.log(Date.now()/1000);
 
 let newDate = new Date()
 console.log(newDate);
@@ -22,3 +26,15 @@ console.log(newDate.getDay());
 newDate.toLocaleString('default',{
     weekday:"long",
 })
+
+
+
+
+
+
+
+
+
+
+
+
