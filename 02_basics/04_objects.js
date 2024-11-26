69020dfa91d3f25544473520ee2c1599b32d684a
ronitjacob <164862978+ronitjacob@users.noreply.github.com>
2024-11-26 14:14:45 +0000

    objects2

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -37,14 +37,44 @@ const user = [
 ]
 
 // users[1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLogged'));
+// console.log(tinderUser.hasOwnProperty('isLogged'));
 
 
 
+
+const course = {
+    coursename: "Js in hindi",
+    price: "999",
+    courseInstructor: "ronit"
+}
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// const navbar = ({company}) =>  {
+
+// }
+// navbar(company = "ronit")
+
+// {
+//     "name": "ronit",
+//     "coursename": "JS in hindi",
+//     "price": "free"
+// }
+
+
+[
+    {},
+    {},
+    {}
+]
 
