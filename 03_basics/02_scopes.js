7c08e9442a94369f5f4018756590c88e432d0dbf
ronitjacob <164862978+ronitjacob@users.noreply.github.com>
2024-11-27 09:10:50 +0000

    scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,56 +1,17 @@
-//var c = 300
-// let a = 300
-// if (true) {
-//     let a = 10
-//     const b = 20
-//     console.log("INNER: ", a);
-// }
-//  for (let i= 0; i < array.length; i++) {
-//     const element = array[i];
+// var c = 300
+let a = 300
+if(true){
+    let a = 10
+    const b = 20
+   console.log(("INNER: ", a));
+   
+}
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
     
-//  }
+}
 
-// console.log(a); 
+ console.log(a);
 // console.log(b);
-// console.log(c);
-
-function one() {
-    const username = "ronit"
-
-    function two(){
-        const website = "youtube"
-        // console.log(username);
-    }
-    // console.log(website);
-
-    two()
-}
-
-// one()
-
-if (true) {
-    const username = "ronit"
-    if (username === "ronit") {
-        const website = " youtube"
-        
-    }
-    // console.log(website);
-}
-// console.log(username);
-
-
-//++++++++++++++++++ interesting +++++++++++++++++
-
-
-addone(5)
-function addone(num) {
-    return num + 1
-}
-
-
-
-addTwo(5)
-const addTwo = function(num){
-    return num  + 2
-}
-
+//  console.log(c);
