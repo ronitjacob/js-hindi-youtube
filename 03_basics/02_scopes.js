38f06ef7a0433fea8d041e3240f8fca1e43e9a63
ronitjacob <164862978+ronitjacob@users.noreply.github.com>
2024-11-27 14:29:20 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -3,15 +3,61 @@ let a = 300
 if(true){
     let a = 10
     const b = 20
-   console.log(("INNER: ", a));
+//    console.log(("INNER: ", a));
    
 }
 
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+    
+// }
+ // console.log(a);
+//  console.log(b);
+//  console.log(c);
+
+function one(){
+    const username= "ronit"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+        
+    }
+    // console.log(website);
+
+    two()
+    
+}
+ 
+// one()
+
+if (true) {
+    const username = "ronit"
+    if (username === "ronit") {
+        const website = " youtube"
+        // console.log(username + website);
+        
+    }
+    // console.log(website);
     
 }
 
- console.log(a);
-// console.log(b);
-//  console.log(c);
+// console.log(username);
+
+
+// ++++++++++++++++++++ interseting +++++++++++++++++++
+
+
+// console.log(addone(5))
+function addone(num){
+       return num + 1
+}
+
+// console.log();
+
+addTwo(5)
+const addTwo = function(num) {
+    return num + 2
+}
+
+
