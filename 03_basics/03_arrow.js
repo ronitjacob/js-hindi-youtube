fa4fca915cef40a94b223919079e73fe2dc13cc2
ronitjacob <164862978+ronitjacob@users.noreply.github.com>
2024-11-27 14:47:17 +0000

    arrow

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -3,47 +3,15 @@ const user = {
     price: 999,
 
     welcomeMessage: function(){
-           console.log(`${this.username} welcome to website`);
-           console.log(this);
-    }
+          console.log(`${this.username} , welcome to website`);
+          console.log(this);
+          
+          }
 
 }
 
 //  user.welcomeMessage()
 //  user.username = "sam"
-// user.welcomeMessage()
+//  user.welcomeMessage()
 
-// console.log(this);
-
-// function chai(){
-//     let username = "ronit"
-//     console.log(this);
-// }
-
-// chai()
-
-// const chai = function () {
-//     let username = "ronit"
-//     console.log(this.username);
-// }
-
-const chai =  () => {
-    let username = "ronit"
-    console.log(this);
-}
-
-
-
-// chai()
-// const addTwo = (num1, num2) => {
-//     return num1 + num2
-// }
-
-// const addTwo = (num1, num2) => num1 + num2
-const addTwo = (num1, num2) => {username: "ronit"}
-
-console.log(addTwo(3, 4))
-
-const myArray = [2,5,3,7,8]
-
-// myArray.forEach()
+console.log(this);
