a2275d42474418a28f990936f8a66865f03d6eac
ronitjacob <164862978+ronitjacob@users.noreply.github.com>
2024-11-29 14:59:05 +0000

    one

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,47 +1,44 @@
 // if
 const isUserLoggedIn = true
 const temperature = 41
+if (temperature < 50) {
+    console.log("executed");
+    
+} else{
+    console.log("temperature is greater than 50");
+}
 
-// if( temperature === 41){
-//      console.log("less than 50");
-// } else {
-//     console.log("temperature is greater than 50");
-// }
-// console.log("Execute");
+
 
 // <, >, <=, >=, ==, !=, ===, !==
 
-// const score = 200
-
 // if (score > 100) {
-//       const power = "fly"
-//       console.log(`User power: ${power}`);
+//     const power = "fly"
+//     console.log(`User power: ${power}`);
+    
 // }
+// console.log(`User power: ${power}`);
 
-//  console.log(`User power: ${power}`);
+// const balance = 1000
 
-const balance = 1000
+// if (balance > 500) {
 
-// if (balance > 500) console.log("test");
-// console.log("test2");
+// }
 
 // if (balance < 500) {
 //     console.log("less than 500");
-// } else if (balance < 750) {
-//     console.log("less than 750");
-
+// } else if (balance < 750){
+//    console.log("less than 750");
 // } else if (balance < 900) {
 //     console.log("less than 750");
-
-// } else {
-//     console.log("less than 1200");
+    
 // }
 
-const userloggedIn = true
-const debitcard = true
-const loggedInFromGoogle = false
-const loggedInFromEmail = true
 
-if (loggedInFromGoogle || loggedInFromEmail) {
-    console.log("User logged in");
+const userLoggedIn = true
+const debitCard = true 
+
+if(userLoggedIn && debitCard) {
+    console.log("Allow to buy course");
+    
 }
