3778a7106e365610f5562370a5c15d6ca22c6907
ronitjacob <164862978+ronitjacob@users.noreply.github.com>
2024-11-30 18:24:51 +0000

    objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,80 +1,62 @@
 // const tinderUser = new Object()
+
 const tinderUser = {}
 
-tinderUser.id = "123abc"
-tinderUser.name = "sammy"
-tinderUser.isLoggedIn = false
+ tinderUser.id = "123abc"
+ tinderUser.name = "ronit"
+ tinderUser.isLoggedIn = false
 
 // console.log(tinderUser);
 
 const regularUser = {
-    email: "some@gmail.com",
+    email: "ronit@gmail.com",
     fullname: {
-          userfullname: {
-             firstname: "ronit",
-             lastname: "jacob"
-          }
+        userfullname: {
+            firstname: "ronit",
+            lastname: "jacob"
+        }
     }
 }
 
-// console.log(regularUser.fullname.userfullname);
+// console.log(regularUser.fullname.userfullname.firstname);
 
 const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "a", 4: "b"}
 const obj4 = {5: "a", 6: "b"}
 
-// const obj3 = {obj1, obj2}
-// const obj3= Object.assign({},obj1, obj2)
+// const obj3 = {obj1 , obj2}
+// const obj3 = Object.assign({},obj1,obj2,obj4)
 
 const obj3 = {...obj1, ...obj2}
 // console.log(obj3);
 
-const user = [
+const users = [
     {
-        id:1,
-        email: "h@google.com"
+        id: 1,
+        email: "r@gmail.com"
+    },
+    {
+        id: 1,
+        email: "r@gmail.com"
+    },
+    {
+        id: 1,
+        email: "r@gmail.com"
     }
 ]
 
-// users[1].email
-// console.log(tinderUser);
+users[1].email
+console.log(tinderUser);
 
-// console.log(Object.keys(tinderUser));
-// console.log(Object.values(tinderUser));
-// console.log(Object.entries(tinderUser));
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
 
-// console.log(tinderUser.hasOwnProperty('isLogged'));
-
-
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
 
 
-const course = {
-    coursename: "Js in hindi",
-    price: "999",
-    courseInstructor: "ronit"
-}
-// course.courseInstructor
-
-const {courseInstructor: instructor} = course
-
-// console.log(courseInstructor);
-console.log(instructor);
-
-// const navbar = ({company}) =>  {
-
-// }
-// navbar(company = "ronit")
-
-// {
-//     "name": "ronit",
-//     "coursename": "JS in hindi",
-//     "price": "free"
-// }
 
 
-[
-    {},
-    {},
-    {}
-]
+
+
 
