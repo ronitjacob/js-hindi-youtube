79255089dd8c57f9ae53a938c00e3edbb4a686d7
ronitjacob <164862978+ronitjacob@users.noreply.github.com>
2024-12-04 14:47:22 +0000

    arrow

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -4,10 +4,45 @@ const user = {
 
     welcomeMessage: function() {
         console.log(`${this.username}, welcome to website`);
+        console.log(this);
         
     }
 }
 
-user.welcomeMessage()
-user.username = "sam"
-user.welcomeMessage()
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username = "ronit"
+//     console.log(this.username);
+// }
+
+// chai()
+
+// const chai = function () {
+//     let username = "ronit"
+//     console.log(this.username);
+// }
+
+const chai =  () => {
+    let username = "ronit"
+    console.log(this);
+}
+
+// chai()
+
+
+// const addTwo =(num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo =(num1, num2) =>  num1 + num2
+
+// const addTwo =(num1, num2) =>  ( num1 + num2 )
+
+const addTwo =(num1, num2) =>  ({username: "ronit"})
+
+console.log(addTwo(3,4))
