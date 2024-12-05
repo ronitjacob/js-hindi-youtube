454fa0d5631d5be992025b2e6b09d0b85dc1c0cb
ronitjacob <164862978+ronitjacob@users.noreply.github.com>
2024-12-05 16:03:57 +0000

    control flow

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -1,42 +1,41 @@
-const userEmail = []
+const userEmail = "r@ronit.ai"
 
 if (userEmail) {
-    // console.log("Got user email");
+    console.log("got user email");
 } else {
-    // console.log("Don't have user email");
-}
+    console.log("Dont have user email");
+} 
 
 // falsy values
 
-// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+// false, 0, -0, BigInt 0n, "", "null", undefined, NaN
 
 // truthy values
-// "0", 'false', " ", [], {}, function(){}
+// "0", 'false, " ", [], {}, function(){}
 
-// if (userEmail.length ===0) {
-//     console.log("Array is empty");
+// if (userEmail.length === 0){
+//      console.log("Array is empty");
+     
 // }
 
-const emptyObj = {}
-
-if (Object.keys(emptyObj).length === 0) {
-    console.log("object is empty");
-}
-
-// Nullish Coalescing Operator (??): null underfined
+// Nullish Coalescing Operatoe ( ??) : null undefined
 
 let val1;
-// val1 = 5 ?? 10 
-// // val1 = null ?? 10
+// val1 = 5 ?? 10
+// val1 = null ?? 10
 // val1 = undefined ?? 15
-val1 = null ?? 10 ?? 20
+val1 = null ?? 10 ?? 10
+
+
 
 
 console.log(val1);
 
-// terniary operator
+// Terniary Operator
 
 // condition ? true : false
 
 const iceTeaPrice = 100
-iceTeaPrice >= 80 ? console.log("less than 80") : console.log("more than 80");
+iceTeaPrice <= 80 ? console.log("less than 80"): console.log("more than 80");
+
+
