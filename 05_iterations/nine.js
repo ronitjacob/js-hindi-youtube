06b69c6c469bfe518352d38a2167f0f47cf5f660
ronitjacob <164862978+ronitjacob@users.noreply.github.com>
2024-12-08 15:03:24 +0000

    nine

--- 05_iterations/nine.js
+++ 05_iterations/nine.js
@@ -2,7 +2,30 @@ const myNums = [1,2,3]
 
 const myTotal = myNums.reduce(function (acc, currval){
     console.log(`acc:${acc} and curravl:${currval}`);
-    return acc + curral
+    return acc + currval
 }, 0)
 
 console.log(myTotal);
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 299
+    },
+    {
+        itemName: "py course",
+        price: 599
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const priceToPay = shoppingCart.reduce((acc,item) => acc + item.price,0)
+
+console.log(priceToPay);
